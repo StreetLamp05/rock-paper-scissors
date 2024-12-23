7a535c9de86b3c38e22f024cb28cdeda3ccde8dc
David Kan <83135817+StreetLamp05@users.noreply.github.com>
2024-12-23 16:38:21 -0500

    initalizing a new git repo

---
+++ assets/js/app.js
@@ -0,0 +1,111 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+let playerScore = 0;
+let computerScore = 0;
+
+
+/**
+ * Generates a random move for the Computer.
+ * @returns The computer's choice of "rock", "paper", or "scissors".
+ */
+function getComputerMove() {    
+    switch (Math.floor(Math.random() * 3)) {
+        case 0:
+            return "rock";
+        case 1: 
+            return "paper";
+        case 2: 
+            return "scissors";
+    } // switch
+} // getComputerMove
+
+
+/**
+ * Calculates the winner between the player and the computer.
+ * 
+ * @param {string} playerMove the player's move.
+ * @param {string} computerMove the computer's move.
+ * @returns The calculated result: "win", "lose", or "draw".
+ */
+function calculateWinner(playerMove, computerMove) {
+    if (playerMove == computerMove) {
+        return "draw";
+    } // if 
+
+    if (
+        (playerMove == "rock" && computerMove == "scissors") ||
+        (playerMove == "paper" && computerMove == "rock") ||
+        (playerMove == "scissors" && computerMove == "paper")
+    ) {
+        return "win";
+    } else {
+        return "lose";
+    } // if
+} // calculateWinner
+
+
+function triggerPlayAnimation() {
+    const playerMoveImg = document.getElementById("player-move");
+    const computerMoveImg = document.getElementById("computer-move");
+
+    playerMoveImg.classList.add("bounce");
+    computerMoveImg.classList.add("bounce");
+
+    setTimeout(() => {
+        playerMoveImg.classList.remove("bounce");
+        computerMoveImg.classList.remove("bounce");
+    }, 900);
+} // triggerPlayAnimation
+
+
+/**
+ * Updates the game's score, changes the move images for the player and computer, and updates the result message label.
+ * 
+ * @param {string} result the result of the round.
+ * @param {string} playerMove the player's move.
+ * @param {string} ComputerMove the computer's move.
+ */
+function updateGameScore(result, playerMove, ComputerMove) {
+    const resultMessageLabel = document.getElementById("result-message");
+    const playerScoreLabel = document.getElementById("player-score");
+    const computerScoreLabel = document.getElementById("computer-score");
+    const playerMoveImg = document.getElementById("player-move");
+    const computerMoveImg = document.getElementById("computer-move");
+
+    if (result == "win") {
+        playerScore++;
+        resultMessageLabel.textContent = "You win!";
+    } else if (result == "lose") {
+        computerScore++;
+        resultMessageLabel.textContent = "You lose!";
+    } else {
+        resultMessageLabel.textContent = "You draw!";
+    } // if
+
+    playerScoreLabel.textContent = playerScore;
+    computerScoreLabel.textContent = computerScore;
+
+    playerMoveImg.src = `/assets/images/left/${playerMove}-left.svg`;
+    computerMoveImg.src = `/assets/images/right/${computerMove}-right.svg`;
+} // updateGame
+
+const buttons = document.querySelectorAll(".choice");
+console.log(buttons);
+buttons.forEach(button => {
+    button.addEventListener("click", (event) => {
+        event.stopPropagation();
+        console.log("Button clicked:", button);
+        const playerMove = button.getAttribute("data-choice");
+        console.log("playerMove: ", playerMove);
+
+        let computerMove = getComputerMove();
+        console.log("computerMove: ", computerMove);
+        let result = calculateWinner(playerMove, computerMove);
+        console.log("result: ", result);
+
+        
+
+    });
+});
+
+})
